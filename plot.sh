42c234bf23dcab46c5761cc2b7b5bfa8831fbafc
andras-kiss <akiss@gamma.ttk.pte.hu>
2017-04-27 22:20:33 +0200

    timer

--- plot.sh
+++ plot.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 echo set grid
 while true; do
-<<<<<<< HEAD
- #echo set yrange \[-2000:2000\]
- echo set xlabel \"time, s\"
- echo set ylabel \"E, mV\"
-=======
->>>>>>> fefabb2232c32f82bd33edeeb9bb807e42c5014a
  sh ./plotline.sh
  #echo plot \""<\(tail -n '-50000' data.dat | sed -e '$ d'\)"\" with lines lc rgb \"red\" notitle
 sleep 1
